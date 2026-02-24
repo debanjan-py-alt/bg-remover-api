de684483f8ef4bc2cfc7225c33d6c1c12865dbd9
debanjan-py-alt <dasdebanjan139@gmail.com>
2026-02-25 00:08:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
 from rembg import remove
+from PIL import Image
 import io
 
 app = FastAPI()
 
 @app.post("/remove")
 async def remove_background(file: UploadFile = File(...)):
-    try:
-        input_bytes = await file.read()
-        output_bytes = remove(input_bytes)
+    input_bytes = await file.read()
 
-        return StreamingResponse(
-            io.BytesIO(output_bytes),
-            media_type="image/png"
-        )
-    except Exception as e:
-        return {"error": str(e)}
+    output_bytes = remove(input_bytes)
+
+    return StreamingResponse(
+        io.BytesIO(output_bytes),
+        media_type="image/png"
+    )
